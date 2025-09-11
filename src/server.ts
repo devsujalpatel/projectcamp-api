560ed4cfe3a8d84c29be04c063c70c8ab61d80d3
devsujalpatel <sujal7455@gmail.com>
2025-09-11 17:39:14 +0530

    Ready to start building routes and models

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from 'express';
-import { connectToDatabase } from './lib/mongoose';
+import { connectToDatabase } from '@/lib/mongoose';
 import config from '@/config';
-import userModel from './models/user.model';
 
 const app = express();
 
@@ -15,44 +14,6 @@ app.get('/', (req: Request, res: Response) => {
   res.end('Hello from project camp');
 });
 
-app.post('/api/v1/users/register', async (req: Request, res: Response) => {
-  const { firstname, lastname, email, password } = req.body as {
-    firstname: string;
-    lastname: string;
-    email: string;
-    password: string;
-  };
 
-  try {
-    const user = await userModel.findOne({
-      email,
-    });
-
-    if (user) {
-      return res.status(400).json({
-        message: 'User already exists',
-      });
-    }
-
-    userModel.create({
-      firstname,
-      lastname,
-      email,
-      password,
-    });
-
-    res.json({
-      firstname,
-      lastname,
-      email,
-      password,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: 'Something went wrong',
-    });
-  }
-});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
