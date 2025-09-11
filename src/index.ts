34fd49fdc61194a997ef34625e3a7a7a89d2aeba
devsujalpatel <sujal7455@gmail.com>
2025-09-11 15:34:51 +0530

    prettier added

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-import express, { Request, Response } from "express"
-import 'dotenv/config'
+import express, { Request, Response } from "express";
+import "dotenv/config";
 
-const app = express()
-
+const app = express();
 
 const PORT = process.env.PORT ?? 8000;
 
-
 app.get("/", (req: Request, res: Response) => {
-    res.end("Hello from project camp")
-})
+  res.end("Hello from project camp");
+});
 
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
