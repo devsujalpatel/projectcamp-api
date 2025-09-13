fea6b657e23ff2636e1424d7d0195d4123245cb0
devsujalpatel <sujal7455@gmail.com>
2025-09-13 23:24:13 +0530

    enums added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,15 @@
-export enum UserRole {
-  USER = "user",
-  ADMIN = "admin",
+export enum UserRoleEnum {
+  MEMBER = 'member',
+  PROJECT_ADMIN = 'project_admin',
+  ADMIN = 'admin',
 }
 
-export type UserRoleType = `${UserRole}`; // "user" | "admin"
+export const AvailableUserRole = Object.values(UserRoleEnum);
+
+
+export enum TaskStatusEnum {
+    TODO = "todo", 
+    IN_PROGRESS = "in_progress",
+    DONE = "done"
+}
+export const AvailableTaskStatus = Object.values(TaskStatusEnum);
