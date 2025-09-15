1f8ef71516491c8172c1f559f5b341fbb8ab2db1
devsujalpatel <sujal7455@gmail.com>
2025-09-15 22:04:57 +0530

    created asynchandler

--- src/controllers/healthcheck.controller.ts
+++ src/controllers/healthcheck.controller.ts
@@ -1,14 +1,10 @@
 import { ApiError } from '@/utils/api-error';
 import { ApiResponse } from '@/utils/api-response';
+import { asyncHandler } from '@/utils/async-handler';
 import { Request, Response } from 'express';
 
-export const healcheck = (req: Request, res: Response) => {
-  try {
-    res
-      .status(200)
-      .json(new ApiResponse({ statusCode: 200, data: {}, message: 'success' }));
-  } catch (error) {
-    res.status(500).json(new ApiError({ statusCode: 500, message: 'error' }));
-  }
-};
-
+export const healcheck = asyncHandler(async (req: Request, res: Response) => {
+  res
+    .status(200)
+    .json(new ApiResponse({ statusCode: 200, data: 'Server is Running' }));
+});
---
+++ src/utils/async-handler.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const asyncHandler = (requestHandler: Function) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
