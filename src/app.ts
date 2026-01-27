e1040c0ed2ae16198214039fbdad83ffb57b7113
Sujal Patel <sujal7455@gmail.com>
2026-01-28 03:42:31 +0530

    cors setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import express from 'express';
 import "dotenv/config"
+import cors from 'cors';
 
-export const app = express();
+const app = express();
 
-app.use(express.json());
+// basic configuration 
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'))
+
+// cors configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}))
 
 // Sample route
 app.get('/', (req, res) => {
     res.end('Hello, World!');
 });
+
+
+export default app;
